1fcdd274f9a5e84df731695ae5e16003368478dd
Podshot <ben.gothard3@gmail.com>
2014-07-25 14:28:56 -0400

    More video stuff

---
+++ video/Filter.py
@@ -0,0 +1,10 @@
+
+UPDATE_URL = "http://podshot.github.io/video/Filter.json"
+VERSION = "2.0.0"
+
+inputs = (
+    ("Updated filter", "label"),
+    )
+
+def perform(level, box, options):
+    raise Exception("Filter now at version 2.0!")
