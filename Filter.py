48eff8ef2dede2b7d61fda56a07152d6de12eeb4
Podshot <ben.gothard3@gmail.com>
2015-01-07 21:47:33 -0500

    Don't mind me, just redoing my Github Site :P

--- Filter.py
+++
@@ -1,6 +0,0 @@
-
-UPDATE_URL = "http://podshot.github.io/video/Filter.json"
-VERSION = "1.0.0"
-
-def perform(level, box, options):
-    return
--- filters/alt/lib/youtube_player.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Tomsik68
- */
-function play(video){
-	var videoLink = "http://www.youtube.com/embed/"+video;
-	document.getElementById('youtube').src = videoLink;
-	setTimeout('$("#videoplayer").show()',200);
-}
